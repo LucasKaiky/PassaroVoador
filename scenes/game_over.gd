c84b5cf517959d299464e9ffd0fbd9a8eeba5f97
LucasHUB360Tech <100947561+LucasKaiky@users.noreply.github.com>
2024-12-03 19:49:58 -0300

    Update

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -4,3 +4,7 @@ signal restart
 
 func _on_restart_button_pressed():
 	restart.emit()
+
+
+func _on_menu_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_how_to_play_button_pressed():
+	$Control/HowToPlayButton/HowToPlayWindow.visible = true
+
+
+func _on_how_to_play_window_close_requested():
+	$Control/HowToPlayButton/HowToPlayWindow.visible = false
